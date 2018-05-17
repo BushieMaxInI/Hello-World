5776130ff7091ad2eddae96eb09a48841a3d6156
iMaxGitHUB <39045207+iMaxGitHUB@users.noreply.github.com>
2018-05-17 22:46:27 +0200

    Scope Identification and using a Class on multiple occassions

--- main.cc
+++ main.cc
@@ -3,15 +3,25 @@
 class World
 {
     public:
-    World() { std::cout << "Hello! \n"; }
-    ~World(){ std::cout << "Good bye \n"; }
+    World(int i) 
+    { 
+        std::cout << "Hello! from " << i << " \n"; 
+    }
+
+    ~World()
+    { 
+        std::cout << "Good bye  \n"; 
+    }
 };
 
-World TheWorld;
+World TheWorld(1);
 
 int main(int argc, char const *argv[])
 {
+    World aWorld(2);
     /* code */
+    std::cout << "Hello from main function \n";
+
     return 0;
 }
 
