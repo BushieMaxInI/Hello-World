68adf34a9092970a2de81d9cd3149f022c47ea66
iMaxTD5 <39045207+iMaxGitHUB@users.noreply.github.com>
2018-05-20 11:51:02 +0200

    Embedded Object done

--- main.cc
+++ main.cc
@@ -1,20 +1,36 @@
 #include <iostream>
 
+class Matter 
+{
+public:
+    Matter(int id) : _identifier(id)
+    {
+        std::cout << " Matter for " << _identifier << " created \n";
+    }
+    ~Matter()
+    {
+        std::cout << " Matter in " << _identifier << " destroyed \n";
+    }
+private:
+    const int _identifier;
+};
+
 class World
 {
     public:
-    World(int id) : _identifier(id)
+    World(int id) : _identifier(id), _matter(_identifier) // initializing embeddings
     { 
-        std::cout << "Hello! from " << _identifier << " \n"; 
+        std::cout << "Hello! from World " << _identifier << " .\n"; 
     }
 
     ~World()
     { 
-        std::cout << "Good bye "<< _identifier << " \n"; 
+        std::cout << "Good bye from World "<< _identifier << " \n"; 
     }
 
     private:
     const int _identifier;
+    const Matter _matter; // Embedded objets of type Matter
 };
 
 World TheWorld(1);
@@ -22,15 +38,17 @@ World TheWorld(1);
 int main(int argc, char const *argv[])
 {
     World aWorld(2);
-
-        for(int i = 3; i < 6; i++)
-    {
-        /* code */
-        World myWorld(i);
-    }
-
-    World oneMoreWorld(6);    
-    /* code */
+//
+//        for(int i = 3; i < 6; i++)
+//   {
+//      
+//       World myWorld(i);
+//    }
+//
+//   World oneMoreWorld(6);    
+//      
+//     
+//
     std::cout << "Hello from main function \n";
 
     return 0;
